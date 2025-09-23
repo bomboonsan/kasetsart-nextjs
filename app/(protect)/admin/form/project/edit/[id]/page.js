6550c8024f944f489a2fc42859afddc12d025bd5
bomboonsan <bomboonsan@gmail.com>
2025-09-23 22:46:12 +0700

    add Project edit

---
+++ app/(protect)/admin/form/project/edit/[id]/page.js
@@ -0,0 +1,11 @@
+"use client"
+
+import React, { useState, useEffect, useMemo } from 'react'
+
+export default function ProjectEdit() {
+    return (
+        <>
+        EDIT
+        </>
+    )
+}
